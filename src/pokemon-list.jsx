afb22cc6e698ea5943eaf7549946f77e76f123aa
rikusrocks <rikusrocks@gmail.com>
2023-07-22 22:59:52 +1000

    solution

--- src/pokemon-list.jsx
+++ src/pokemon-list.jsx
@@ -2,6 +2,43 @@
 // Make sure to replace limit and offset with the appropriate values
 // https://pokeapi.co/api/v2/pokemon?limit=5&offset=0
 
-const PokemonList = () => {};
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+const baseUrl = "https://pokeapi.co/api/v2/pokemon?limit=5&offset=0"
+const PokemonList = () => {
+    const [pokemonInfo, setPokemonInfo] = useState({})
+    const [pokemonShowed, setPokemonShowed] = useState([])
+
+    useEffect(() => {
+        getPokemonInfo()
+    },[])
+
+    const getPokemonInfo = async (url = baseUrl) => {
+        const response = await fetch(url)
+        const pokeInfo = await response.json()
+        setPokemonInfo(pokeInfo)
+        setPokemonShowed([...pokemonShowed, ...pokeInfo.results])
+    }
+
+    const loadMorePokemon = async () => {
+        getPokemonInfo(pokemonInfo.next)
+    }
+    return (
+        <>
+        <ul>
+        {pokemonShowed && pokemonShowed.map((pokemon, index) => (
+            <li key={index}>
+                {pokemon.name}
+            </li>
+        ))}
+        </ul>
+        {pokemonInfo && <div>{`Displaying ${pokemonShowed.length} of ${pokemonInfo.count} results`}</div>}
+        {pokemonShowed.length !== pokemonInfo.count && <button onClick={loadMorePokemon}>Load more</button>}
+    </>
+    )
+
+};
 
 export default PokemonList;
